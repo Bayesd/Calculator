72d9ae2679c55565665ae01195fccb615562c413
Bayesd <68394122+Bayesd@users.noreply.github.com>
2020-09-13 18:57:46 +0200

    Calcutator uppgift 1
    Klarar de 4 räknesätten, avrundar resultat i heltal till int om det är möjligt för att ta bort kommatecknet.

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+import java.util.Sc
+
+public class Calculator {
+    public static void main(String[] args ) {
+        boolean isRunning = true;
+
+        while (isRunning) {
+        }
+
+    }
+
+    public static double addition(double x, double y) {
+        double result = 0.0;
+        return result;
+    }
+
+    public static double subtraction(double x, double y) {
+        double result = 0.0;
+        return result;
+    }
+
+    public static double multiplication(double x, double y) {
+        double result = 0.0;
+        return result;
+    }
+
+    public static double division(double x, double y) {
+        double result = 0.0;
+        return result;
+    }
+}
